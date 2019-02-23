1a0fd3a3c284c6538055cb5bda41a1e42b6a3079
aplenkina <aplenkina>
2019-02-24 00:52:07 +0300

    Add levelsTail

--- water_level.php
+++ water_level.php
@@ -7,13 +7,13 @@ $levelMax = 100000000;
 $array = getArray($nMax, $levelMax);
 $waterMax = getMaxWaterLevel($array);
 
-echo 'Max value: ' . $waterMax;
-exit(0);
+echo 'All leveles: ' . implode(', ', $array) . '; Max value: ' . $waterMax;
 
 function getArray($nMax, $levelMax) {
 	if (!$nMax || !$levelMax) {
 		return [];
 	}
+
 	$n = rand(1, abs($nMax));
 	for ($i = 0; $i <= $n; $i++) {
 		$array[] = rand(1, abs($levelMax));
@@ -22,22 +22,30 @@ function getArray($nMax, $levelMax) {
 }
 
 function getMaxWaterLevel($array) {
+	
+	$count = count($array);
 	$max = 0;
 	$min = 0;
 	$levels = [];
-	$count = count($array);
+	$levelsTail = [];
 	for ($i = 1; $i < $count; $i++) {
-
 		if ($array[$i - 1] <= $array[$i]) {
 			continue;
 		}
 
 		$max = $array[$i - 1];
 		$min = $array[$i];
+		$subArray = [];
 
 		while ($i < $count && $max > $array[$i]) {
-			if ($min > $array[$i]) {
+			if ($min >= $array[$i]) {
 				$min = $array[$i];
+			}
+			$subArray[$i] = $array[$i];
+			if (isset($array[$i + 1]) && $array[$i + 1] > $array[$i] && $array[$i + 1] < $max) {
+				$levelsTail[] = $array[$i + 1] - min($subArray);
+			} elseif (isset($array[$i + 1]) && $array[$i + 1] < $array[$i]) {
+				$subArray = [];
 			}
 			$i++;
 		}
@@ -46,6 +54,7 @@ function getMaxWaterLevel($array) {
 			$levels[] = $max - $min;
 		}
 	}
-	return ($levels) ? max($levels) : 0;
-}
+	$allMaxValues = array_merge($levels, $levelsTail);
 
+	return ($allMaxValues) ? max($allMaxValues) : 0;
+}
